7dc31458c46795589689f77300a3575e78be08d5
Rafael Fernandes de Oliveira <rafaelolivei@gmail.com>
2019-06-02 22:46:57 -0300

    Conclusão de CRUD RestFul

--- RestWithASPNETudemy/RestWithASPNETudemy/Controllers/PersonController.cs
+++ RestWithASPNETudemy/RestWithASPNETudemy/Controllers/PersonController.cs
@@ -1,4 +1,6 @@
 ﻿using Microsoft.AspNetCore.Mvc;
+using RestWithASPNETudemy.Models;
+using RestWithASPNETudemy.Services;
 using System.Collections.Generic;
 
 namespace RestWithASPNETudemy.Controllers
@@ -7,6 +9,60 @@ namespace RestWithASPNETudemy.Controllers
     [ApiController]
     public class PersonController : ControllerBase
     {
-        
+        private IPersonService _personService;
+
+        public PersonController(IPersonService personService)
+        {
+            _personService = personService;
+        }
+
+        [HttpGet]
+        public IActionResult Get()
+        {
+            var persons = _personService.GetAll();
+            return Ok(persons);
+        }
+
+        [HttpGet("{id}")]
+        public IActionResult Get(long id)
+        {
+            var person = _personService.FindById(id);
+
+            if (person == null)
+                return NotFound($"Person not found with id {id}");
+
+            return Ok(person);
+        }
+
+        [HttpPost]
+        public IActionResult Post([FromBody] Person person)
+        {
+            if (person == null)
+                return BadRequest("Invalid person");
+
+            return new CreatedResult("", _personService.Create(person));
+        }
+
+        [HttpPut]
+        public IActionResult Put([FromBody] Person person)
+        {
+            if (person == null)
+                return BadRequest("Invalid person");
+
+            return new ObjectResult(_personService.Update(person));
+        }
+
+        [HttpDelete]
+        public IActionResult Delete(long id)
+        {
+            var person = _personService.FindById(id);
+
+            if (person == null)
+                return NotFound();
+
+            _personService.Delete(id);
+
+            return NoContent();
+        }
     }
 }
--- RestWithASPNETudemy/RestWithASPNETudemy/Startup.cs
+++ RestWithASPNETudemy/RestWithASPNETudemy/Startup.cs
@@ -10,6 +10,8 @@ using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Logging;
 using Microsoft.Extensions.Options;
+using RestWithASPNETudemy.Services;
+using RestWithASPNETudemy.Services.Implementation;
 
 namespace RestWithASPNETudemy
 {
@@ -26,6 +28,8 @@ namespace RestWithASPNETudemy
         public void ConfigureServices(IServiceCollection services)
         {
             services.AddMvc().SetCompatibilityVersion(CompatibilityVersion.Version_2_2);
+
+            services.AddScoped<IPersonService, PersonService>();
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
