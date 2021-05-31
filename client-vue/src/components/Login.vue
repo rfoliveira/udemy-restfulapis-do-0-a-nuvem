0c25146d7eaceee47812fe93b5133a38682b96b1
Rafaer Fernandes de Oliveira <rafaelolivei@gmail.com>
2021-05-31 15:04:48 -0300

    Início projeto client em vuejs

---
+++ client-vue/src/components/Login.vue
@@ -0,0 +1,34 @@
+<template>
+  <div className="login-container">
+    <section className="form">
+        <img :src="logo_img" alt="REST API Logo" />
+
+        <form @submit.prevent="() => console.log('vai submmitar')">
+            <h1>Access your account</h1>
+            
+            <input placeholder="Username" 
+                :value="username"
+                @change="setUsername" />
+
+            <input type="password" 
+                placeholder="Password" 
+                :value="password" 
+                @change="setPassword" />
+
+            <button className="button" type="submit">Login</button>
+        </form>
+    </section>
+
+    <img :src="padlock_img" alt="Login" />
+</div>
+</template>
+
+<script>
+export default {
+
+}
+</script>
+
+<style>
+
+</style>
